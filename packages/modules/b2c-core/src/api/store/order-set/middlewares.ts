7dd19341e6c969d26a6652cf84a6bb60adee1c48
Alejandro Ruggeri <alejandro.ruggeri.l@gmail.com>
2026-05-03 01:28:29 +0200

    fix(store): guard order set ownership

--- packages/modules/b2c-core/src/api/store/order-set/middlewares.ts
+++ packages/modules/b2c-core/src/api/store/order-set/middlewares.ts
@@ -1,5 +1,10 @@
-import { MiddlewareRoute, validateAndTransformQuery } from '@medusajs/framework'
+import {
+  MiddlewareRoute,
+  authenticate,
+  validateAndTransformQuery
+} from '@medusajs/framework'
 
+import { checkCustomerResourceOwnershipByResourceId } from '../../../shared/infra/http/middlewares'
 import { orderSetQueryConfig } from './query-config'
 import { StoreGetOrderSetParams } from './validators'
 
@@ -8,6 +13,7 @@ export const storeOrderSetMiddlewares: MiddlewareRoute[] = [
     method: ['GET'],
     matcher: '/store/order-set',
     middlewares: [
+      authenticate('customer', ['bearer', 'session']),
       validateAndTransformQuery(
         StoreGetOrderSetParams,
         orderSetQueryConfig.list
@@ -18,10 +24,14 @@ export const storeOrderSetMiddlewares: MiddlewareRoute[] = [
     method: ['GET'],
     matcher: '/store/order-set/:id',
     middlewares: [
+      authenticate('customer', ['bearer', 'session']),
       validateAndTransformQuery(
         StoreGetOrderSetParams,
         orderSetQueryConfig.retrieve
-      )
+      ),
+      checkCustomerResourceOwnershipByResourceId({
+        entryPoint: 'order_set'
+      })
     ]
   }
 ]
